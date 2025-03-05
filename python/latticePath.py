5e2fed72c55bdc37e1ff39d69c59ee7dbacc0b9a
cynthion21x <cynthion21x@gmail.com>
2025-03-05 16:30:41 +0000

    finished latticepath

--- python/latticePath.py
+++ python/latticePath.py
@@ -9,3 +9,24 @@ pascal = []
 for i in range(0, 21):
 
     pascal.append(choose(20, i))
+
+def NextR(x):
+
+    r = []
+    for n in range(1, len(x)-1):
+        r.append(x[(n-1)]+2*x[n]+x[(n+1)])
+
+    return r
+
+ans = NextR(pascal)
+print(ans)
+
+while len(ans) != 1:
+
+    if len(ans) == 2:
+        ans = 2 * ans[0]
+        break
+
+    ans = NextR(ans)
+
+print(ans)
